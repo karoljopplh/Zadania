5d3d1207d33578139f2598e1f0bdf18320568947
karoljopplh <151885159+karoljopplh@users.noreply.github.com>
2023-11-24 08:57:19 +0100

    Add files via upload

--- Tab2w/tab2w_gotoxy.cpp
+++ Tab2w/tab2w_gotoxy.cpp
@@ -6,11 +6,21 @@
 #define YY 3
 /**/
 
+void gotoxy(int x, int y)
+{
+	COORD c;
+	c.X = x-1;
+	c.Y = y-1;
+	SetConsoleCursorPosition (GetStdHandle (STD_OUTPUT_HANDLE), c);
+}
+
 using namespace std;
 int main()
 {
 	int tab[XX][YY], ix, iy;
+	gotoxy(28, 1);
 	cout<<"Testowanie tablic 2W";
+	gotoxy(1,2);
 	for(iy=0; iy<YY; iy++)
 	{
 		for(ix=0; ix<XX; ix++)
@@ -19,7 +29,7 @@ int main()
 			cin>>tab[ix][iy];
 		}
 	}
-	
+	gotoxy(1,15);
 	cout<<"Ponizej listing kontrolny:"<<endl;
 	for(iy=0; iy<YY; iy++)
 	{
