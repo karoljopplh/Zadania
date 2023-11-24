de9e797ad75b0f3a45b12be90a9b3ad8170ff775
karoljopplh <151885159+karoljopplh@users.noreply.github.com>
2023-11-24 08:51:48 +0100

    Add files via upload

--- Tab2w/tab2w1.cpp
+++ Tab2w/tab2w1.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
+#include <cstdlib>
+#include <windows.h>
+
 #define XX 4
 #define YY 3
 /**/
+
+void gotoxy(int x, int y)
+{
+	COORD c;
+	c.X = x-1;
+	c.Y = y-1;
+	SetConsoleCursorPosition (GetStdHandle (STD_OUTPUT_HANDLE), c)
+}
 
 using namespace std;
 int main()
 {
 	int tab[XX][YY], ix, iy;
-	
+	goto()
+	cout<<"Testowanie tablic 2W";
 	for(iy=0; iy<YY; iy++)
 	{
 		for(ix=0; ix<XX; ix++)
