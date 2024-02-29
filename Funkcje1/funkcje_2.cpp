9ddb62fe949aab5d1b05307bb68448ae073c5c85
karoljopplh <151885159+karoljopplh@users.noreply.github.com>
2024-02-29 21:27:23 +0100

    Add files via upload

--- Funkcje1/funkcje_2.cpp
+++ Funkcje1/funkcje_2.cpp
@@ -10,6 +10,8 @@ void klawisz2();
 float obw_prost1(float a, float b);
 float obw_prost2(float a, float b);
 int silnia(int n);
+unsigned long long silnia_rec(unsigned int n);	//Silnia obliczana funkcją rekurencyjna
+float pole_prost(float a, float b);
 
 int main() 
 {
@@ -35,9 +37,26 @@ int main()
 	else
 		cout<<"ERROR"<<endl;
 	
+	cout<<"Obliczanie pola prostokata"<<endl;
+	pole = pole_prost(bok1, bok2);
+	if(pole>0)
+		cout<<"Pole prostokata = "<<pole<<endl;
+	else if(pole == -1)
+		cout<<"ERROR - a niepoprawne"<<endl;
+	else if(pole == -2)
+		cout<<"ERROR - b niepoprawne"<<endl;
+	else if(pole == -3)
+		cout<<"ERROR - obie (a i b) niepoprawne"<<endl;
+	else
+		cout<<"ERROR"<<endl;
+	
 	cout<<endl<<"Obliczanie silni"<<endl;
 	cout<<"Podaj n = ";
-	cin>>n
+	cin>>n;
+	if(silnia(n)==-1)
+		cout<<"ERROR - n niepoprawne (n<0)"<<endl;
+	else
+		cout<<silnia(n);
 	klawisz2();
 	return 0;
 }
@@ -95,13 +114,37 @@ float obw_prost2(float a, float b)	//-1 gdy pierwszy parametr, -2 gdy 2, -3 gdy 
 
 int silnia(int n)
 {
-	/*
+	int wynik=1;
 	if(n<0)
-		return -1;	//n ujemne
-	if(n==0)
-		return 0;	//n == 0 0*0=0
-	*/
-	for(int wynik=1; n>1; n--)
+		return -1;	//n ujemne - ERROR
+	if(n==0 || n==1)
+		return 1;	//1! == 0! == 1
+	for(; n>1; n--)
 		wynik*=n;
 	return wynik;
+}
+
+unsigned long long silnia_rec(int n)	//Silnia obliczana funkcją rekurencyjna
+{
+	if(n<0)
+		return -1;	//n ujemne - ERROR
+    // Obsługa przypadku dla n równego 0 lub 1
+    if (n == 0 || n == 1) {
+        return 1;
+    } else {
+        // Obliczanie silni dla n większego od 1
+        return n * silnia(n - 1);
+    }
+}
+
+float pole_prost(float a, float b)
+{
+    // Obliczanie pola prostokąta
+    if(a<=0 && b<=0)
+		return -3;	// bad a and b
+	if(b<=0 && a>0)
+		return -2;	//bad b
+	if(a<=0 && b>0)
+		return -1;	//bad a
+    return a*b;
 }
