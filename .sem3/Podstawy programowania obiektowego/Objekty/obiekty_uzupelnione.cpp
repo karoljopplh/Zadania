c4c34564152f89c31b798aac172b9f9bacc7222b
karoljopplh <karoljopplh2@gmail.com>
2024-10-07 14:28:44 +0200

    finished a file after class

--- .sem3/Podstawy programowania obiektowego/Objekty/obiekty_uzupelnione.cpp
+++ .sem3/Podstawy programowania obiektowego/Objekty/obiekty_uzupelnione.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <cstdlib>
+#include <conio.h>
+#include <string.h>
 
 using namespace std;
 class towar
@@ -34,7 +37,7 @@ void towar::czysc()
 	sztuk=0;
 }
 
-void towar::setNazwa(char *naz);
+void towar::setNazwa(char *naz)
 {
 	strcpy(nazwa,naz);
 }
@@ -52,12 +55,19 @@ bool towar::setCena(float cena)
 	}
 }
 
-void towar::setSztuk()
+void towar::setSztuk(int szt)
 {
-	
+	sztuk=szt;	
 }
 
-towar::getNazwa()
+void towar::pobierz(char *naz, float cen, int szt)
+{
+	strcpy(nazwa,naz);
+	cena=cen;
+	sztuk=szt;
+} 
+
+char* towar::getNazwa()
 {
 	return nazwa;
 }
@@ -87,8 +97,48 @@ int main(int argc, char** argv) {
 	{
 		cout<<"1.czyszczenie"<<endl;
 		cout<<"2.czytanie"<<endl;
+		cout<<"3.wypisanie"<<endl;
 		cout<<"4.wartosc"<<endl;
-		cout<<""
+		cout<<"6.setNazwa"<<endl;
+		cout<<"7.setCena"<<endl;
+		cout<<"8.setSztuk"<<endl;
+		cout<<"9.getCena"<<endl;
+		cout<<"5.koniec"<<endl<<endl;
+	
+	zn=getch();
+	switch(zn)
+	{
+		case'1': 	t1.czysc();
+					break;
+		case'2':	{
+						cout<<"nazwa: ";	gets(n);
+						cout<<"cena: ";		cin>>c;
+						cout<<"sztuk: ";	cin>>sz;
+						cin.ignore();
+						t1.pobierz(n,c,sz);	//lub seterami
+						break;
+					}
+		case'3':	cout<<"Nazwa towaru:	"<<t1.getNazwa()<<endl;
+					cout<<"Cena towaru:		"<<t1.getCena()<<endl;
+					cout<<"Liczba sztuk:	"<<t1.getSztuk()<<endl<<endl;
+					break;
+		case'4':	cout<<t1.wartosc()<<endl;
+					break;
+		case'6':	t1.setNazwa("Nowa nazwa");
+					break;
+		case'7':	cout<<"cena: "; cin>>c;
+					cin.ignore();
+					if(!t1.setCena(c))
+						cout<<"Zla cena! Nie ustawiono ceny"<<endl;
+					break;
+		case'8':	cout<<"Nazwa towaru:	"<<t1.getNazwa()<<endl;
+					break;
+		case'9':	cout<<"Cena towaru:	"<<t1.getCena()<<endl;
+					break;
+		case'5':	cout<<"do widzenia"<<endl;
+					getchar();
 	}
+	}
+	while(zn!='5');
 	return 0;
 }
