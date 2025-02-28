5cd531557dc275fd61fe6eaa31515634a54e95b3
karoljopplh <karoljopplh2@gmail.com>
2025-02-28 10:13:48 +0100

    Update Class1.java

--- .sem4/Projektowanie Obiektowe/p1_4ADI/src/p1_4ADI/Class1.java
+++ .sem4/Projektowanie Obiektowe/p1_4ADI/src/p1_4ADI/Class1.java
@@ -13,7 +13,35 @@ public class Class1
 
 	public static void main(String[] args) 
 	{
-		// TODO Auto-generated method stub
+		double wynikd, d;
+		d=5.75;
+		int wynik, i=(int) d;
+		int liczba;
+		Random los;
+		los=new Random();
+		Scanner czytaj=new Scanner(System.in);
+		boolean bool=true;
+		final int stała=100;
+		
+		System.out.println("Napis testowy");
+		System.out.println("\n\tWartość zmiennej i=" + i);
+		System.out.println("sklejone wartości d oraz i =" + d + i);
+		System.out.println("suma d + i +" + (d+i));
+		wynik= (int) d+i;
+		System.out.println("zmienna wynik=" + wynik);
+		wynikd=d+i;
+		System.out.println("zmienna wynikd=" + wynikd);
+		liczba=los.nextInt();
+		System.out.println("losowana=" + liczba);
+		liczba=los.nextInt(2);
+		System.out.println("losowana mała=" + liczba);
+		d=los.nextDouble();
+		d=d*10000;
+		System.out.println("losowana double=" + d);
+		System.out.println("Wprowadz liczbę całkowitą       ");
+		i=czytaj.nextInt();
+		System.out.println("Wprowadziłeś    " + i);
+		czytaj.close();
 
 	}
 
